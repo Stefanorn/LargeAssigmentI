3d9628074761a24009a89d655891e78ac1b175dd
vidir17 <vidir17@ru.is>
2019-09-10 12:52:42 +0000

    Data provider, hardkodud gogn

---
+++ TechnicalRadiation.Repositories/Data/DataProvider.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using TechnicalRadiation.Models.Entities;
+
+
+namespace TechnicalRadiation.Repositories.Data
+{
+    public class DataProvider
+    {
+        //newsitems
+        private static List<NewsItem> newsItems = new List<NewsItem>
+        {
+            new NewsItem
+            {
+                Id = 1,
+                Title = "Þurfa að synda í gegn­um stór­an olíuflekk",
+
+                ImgSource = "https://cdn.mbl.is/frimg/1/15/70/1157040.jpg",
+                shortDescription = "Fólk að synda"
+                //_links,
+            },
+            new NewsItem
+            {
+                Id = 2,
+                Title = "Þurfa að synda í gegn­um stór­an olíuflekk",
+
+                ImgSource = "https://cdn.mbl.is/frimg/1/15/70/1157040.jpg",
+                shortDescription = "Fólk að synda"
+                //_links,
+            },
+            new NewsItem
+            {
+                Id = 3,
+                Title = "Þurfa að synda í gegn­um stór­an olíuflekk",
+
+                ImgSource = "https://cdn.mbl.is/frimg/1/15/70/1157040.jpg",
+                shortDescription = "Fólk að synda"
+                //_links,
+            },
+        },
+        //authors
+        private static List<NewsItemAuthor> NewsItemAuthors = new List<NewsItemAuthor>
+        {
+            new NewsItemAuthor
+            {
+                AuthorId = 1,
+                NewsItemId = 1
+
+            },
+            new NewsItemAuthor
+            {
+                AuthorId = 2,
+                NewsItemId = 2
+
+            },
+            new NewsItemAuthor
+            {
+                AuthorId = 3,
+                NewsItemId = 3
+
+            },
+        },
+        //categories
+        private static List<NewsItemCategories> NewsItemCategoriess = new List<NewsItemCategories>
+        {
+            new NewsItemCategories
+            {
+                CategoryId = 1,
+                NewsItem = 1
+            },
+        }
+    }
+}
